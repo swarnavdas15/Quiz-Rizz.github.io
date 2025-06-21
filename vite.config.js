828345c81a8da17036f7341f28aa794addace0f9
Swarnav Das <swarnavofcl15@gmail.com>
2025-06-22 00:28:22 +0530

    Update Vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base:'/Quizzo-Reactbased.github.io.git/'
+  base:'/Quizzo-Reactbased.github.io/'
 })
