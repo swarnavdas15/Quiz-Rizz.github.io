1d2267e25512995c7ecdd2770c95c22ceae589c6
Swarnav Das <swarnavofcl15@gmail.com>
2025-06-21 22:23:12 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-
-
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import UserForm from './components/UserForm';
